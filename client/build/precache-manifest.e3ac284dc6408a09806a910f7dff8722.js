57facc86fa5c2d762a3d22926a2845b849171756
trome23 <romennoodles23@yahoo.com>
2019-06-19 21:28:09 -0600

    initial commit

---
+++ client/build/precache-manifest.e3ac284dc6408a09806a910f7dff8722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb182142ab68ad5bb5b2",
+    "url": "/static/js/main.abfb18f7.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "cb182142ab68ad5bb5b2",
+    "url": "/static/css/main.63b35e86.chunk.css"
+  },
+  {
+    "revision": "ac833c8a1dc17a6847452e52f4ff39db",
+    "url": "/index.html"
+  }
+];
